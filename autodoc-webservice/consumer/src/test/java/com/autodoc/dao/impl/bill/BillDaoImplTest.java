4ff1ed21dd702664b91eb0bb86d3a6463cab433c
xbreizh <xbreizh@yahoo.co.nz>
2020-01-25 00:53:32 +0000

    added create Bill test on billDaoTest

--- autodoc-webservice/consumer/src/test/java/com/autodoc/dao/impl/bill/BillDaoImplTest.java
+++ autodoc-webservice/consumer/src/test/java/com/autodoc/dao/impl/bill/BillDaoImplTest.java
@@ -1,9 +1,17 @@
 package com.autodoc.dao.impl.bill;
 
 import com.autodoc.dao.contract.bill.BillDao;
+import com.autodoc.dao.contract.car.CarDao;
+import com.autodoc.dao.contract.person.employee.EmployeeDao;
+import com.autodoc.dao.contract.tasks.TaskDao;
 import com.autodoc.dao.filler.Filler;
+import com.autodoc.model.enums.Status;
 import com.autodoc.model.models.bill.Bill;
+import com.autodoc.model.models.car.Car;
+import com.autodoc.model.models.employee.Employee;
+import com.autodoc.model.models.tasks.Task;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -11,6 +19,9 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -31,6 +42,12 @@ BillDaoImplTest {
     private BillDao dao;
     @Inject
     private Filler filler;
+    @Inject
+    private CarDao carDao;
+    @Inject
+    private EmployeeDao employeeDao;
+    @Inject
+    private TaskDao taskDao;
 
     @BeforeEach
     void init() throws Exception {
@@ -38,6 +55,26 @@ BillDaoImplTest {
         bill = (Bill) dao.getAll().get(0);
         id = bill.getId();
 
+    }
+
+
+    @Test
+    @DisplayName("should add bill")
+    void create() {
+        assertEquals(3, dao.getAll().size());
+        Date date = new Date();
+        Status status = Status.PENDING_PIECES;
+        Car car = (Car) carDao.getAll().get(0);
+        Employee employee = (Employee) employeeDao.getAll().get(0);
+        List<Task> tasks = new ArrayList<>();
+        Task task = (Task) taskDao.getAll().get(0);
+        tasks.add(task);
+        double total = 129.98;
+        double vat = 20.00;
+        double discount = 00;
+        Bill bill = new Bill(date, status, car, employee, car.getClient(), tasks, total, vat, discount);
+        dao.create(bill);
+        assertEquals(4, dao.getAll().size());
     }
 
     @Test
