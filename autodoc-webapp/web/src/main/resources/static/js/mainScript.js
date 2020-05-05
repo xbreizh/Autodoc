c760632ddb3eaf29688080623b6fbd0bc083fc84
xbreizh <xbreizh@yahoo.co.nz>
2020-05-05 15:21:36 +0100

    redesign and added clock

--- autodoc-webapp/web/src/main/resources/static/js/mainScript.js
+++ autodoc-webapp/web/src/main/resources/static/js/mainScript.js
@@ -33,6 +33,28 @@ function showLoans() {
     x.style.display = "block";
 }
 
+function startTime() {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('clock').innerHTML =
+        mm + '/' + dd + '/' + yyyy + " " + h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+}
+
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i
+    }  // add zero in front of numbers < 10
+    return i;
+}
+
 
 function alertOverdue() {
     Swal.fire({
